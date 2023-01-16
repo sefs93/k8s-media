e69e4135ca737eb8ddcb6ddf9106b463e34adcdf
sefs93 <sefs93@gmail.com>
2023-01-16 16:23:51 -0500

    Shuffle files

--- boot.sh
+++ boot.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
 sudo snap install microk8s --classic
 microk8s enable dns
 microk8s enable dashboard
 microk8s config > ~/.kube/config
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f argocd-install.yaml 
+microk8s kubectl create namespace argocd
+microk8s kubectl apply -n argocd -f bootstrap/argocd-install.yaml 
+helm apply mediacenter mediacenter
 # kubectl get pods -n argocd | grep argocd-server #Pod name is pass, user: admin # it's in argocd-initial-admin-secret secret
 kubectl get secrets/argocd-initial-admin-secret -n argocd --template={{.data.password}} | base64 -d
