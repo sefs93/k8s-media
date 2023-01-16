38df89752ff82426483f8db57b5c06ede9a083f3
sefs93 <sefs93@gmail.com>
2023-01-16 14:45:08 -0500

    3

--- boot.sh
+++ boot.sh
@@ -3,6 +3,7 @@ microk8s enable dns
 microk8s enable dashboard
 microk8s config > ~/.kube/config
 
-kubectl create namespace argocd; kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+kubectl apply -n argocd -f argocd-install.yaml 
 # kubectl get pods -n argocd | grep argocd-server #Pod name is pass, user: admin # it's in argocd-initial-admin-secret secret
- kubectl get secrets/argocd-initial-admin-secret -n argocd --template={{.data.password}} | base64 -d
+kubectl get secrets/argocd-initial-admin-secret -n argocd --template={{.data.password}} | base64 -d
